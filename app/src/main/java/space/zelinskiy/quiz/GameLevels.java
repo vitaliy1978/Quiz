058347bdef71b0afe96051754dc461a444672572
Vitaliy <vitaliy1978@mail.ru>
2021-04-10 15:35:22 +0600

    10.04.2021_15.34

---
+++ app/src/main/java/space/zelinskiy/quiz/GameLevels.java
@@ -0,0 +1,65 @@
+package space.zelinskiy.quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        //кнопка для перехона 1-й уровень - Начало
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+       textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this,Level1.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+        //кнопка для перехона 1-й уровень - Конец
+    }
+
+    //системная кнопка Назад - начало
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(GameLevels.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+
+        }
+    }
+    //системная кнопка Назад - конец
+}
---
+++ app/src/main/java/space/zelinskiy/quiz/Level1.java
@@ -0,0 +1,132 @@
+package space.zelinskiy.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
+
+    public int numLeft;  //Переменная для левой картинки + текст
+    public int numRight;  //Переменная для правой картинки + текст
+    Array array = new Array(); //Создали новый оъект из класса Array
+    Random random = new Random(); //для генерации случайных чисел
+    public int count =0;  //Счетчик правильных ответов
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal);
+
+        TextView text_levels = (TextView)findViewById(R.id.text_levels);
+        text_levels.setText(R.string.level1);
+
+        final ImageView img_left = (ImageView) findViewById(R.id.img_left);
+        final ImageView img_right = (ImageView) findViewById(R.id.img_right);
+        img_left.setClipToOutline(true);//код, который скругляет углы
+        img_right.setClipToOutline(true);//код, который скругляет углы
+
+        final TextView text_left = findViewById(R.id.text_left);  //Путь к левой TextView
+        final TextView text_right = findViewById(R.id.text_right);  //Путь к правой TextView
+
+        //Развернуть игру на весь экран - Начало
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //Развернуть игру на весь экран - Конец
+
+        dialog = new Dialog(this); //Создам новое диалоговое окно в начале игры
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);  //Скрываем заголовок у диалогового окна
+        dialog.setContentView(R.layout.preview_dialog); //Путь к макету диалогового окна
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); //Прозрачный фон диалогового окна
+        dialog.setCancelable(false);  //окно нельзя открыть кнопкой назад
+        // Кнопка которая закрывает диалоговое окно - Начало
+        TextView button_close = (TextView) dialog.findViewById(R.id.button_close);
+        button_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Level1.this,GameLevels.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) {
+
+                }
+                dialog.dismiss();  //закрываем диалоговое окно
+            }
+        });
+        // Кнопка которая закрывает диалоговое окно - Конец
+
+        //Кнопка Продолжить - Начало
+        Button btn_continue = (Button)dialog.findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();  //закрываем диалоговое окно
+            }
+        });
+        //Кнопка Продолжит - Конец
+
+        dialog.show();  //показать диалоговое окно
+
+        //Кнопка Назад - Начало
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Level1.this,GameLevels.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+        //Кнопка Назад - Конец
+
+        //Подключаем анимацию - начало
+        final Animation a = AnimationUtils.loadAnimation(Level1.this,R.anim.alpha);
+        //Подключаем анимацию - конец
+
+        numLeft=random.nextInt(10); //генерируем случайное число от 0 до 9
+        img_left.setImageResource(array.images1[numLeft]);  //достаем из массива картинку
+        text_left.setText(array.texts1[numLeft]);  //достаем из массива текст
+
+        numRight=random.nextInt(10); //генерируем случайное число от 0 до 9
+        //Цикл проверяющий равенство чисел - Начало
+        while(numLeft==numRight){
+            numRight=random.nextInt(10);
+        }
+        //Цикл проверяющий равенство чисел - Конец
+        img_right.setImageResource(array.images1[numRight]);  //достаем из массива картинку
+        text_right.setText(array.texts1[numRight]);  //достаем из массива текст
+
+    }
+    //системная кнопка Назад - начало
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(Level1.this,GameLevels.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+
+        }
+    }
+    //системная кнопка Назад - конец
+}
